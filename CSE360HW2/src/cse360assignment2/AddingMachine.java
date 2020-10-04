0594249c9e7c50bd14ad105165e37dac1e7e866f
Travis Andring <>
2020-10-04 01:11:40 -0700

    Initial Upload of File

---
+++ CSE360HW2/src/cse360assignment2/AddingMachine.java
@@ -0,0 +1,47 @@
+package cse360assignment2;
+
+public class AddingMachine {
+	private int total;
+	
+	/**
+	 *   Initializes the total variable to 0
+	 */
+	public AddingMachine () {
+	  total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total
+	 * @return
+	 */
+	public int getTotal () {
+	  return 0;
+	}
+	
+	/**
+	 *   Adds the parameter to the total variable
+	 * @param value adds the parameter to the total variable
+	 */
+	public void add (int value) {
+	}
+	
+	/**
+	 * Subtracts the parameter to from the total variable
+	 * @param value subtracts the parameter from the total variable
+	 */
+	public void subtract (int value) {
+	}
+
+	/**
+	 *  Returns a history of the transactions in a string format
+	 */
+	public String toString () {
+	  return "";
+	}
+	
+	/**
+	 * Clears the memory
+	 */
+	public void clear() {
+	}
+}
