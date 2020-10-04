deb72c94b336ded423df346feda93d477764f67a
Travis <Travis@Travis-ROG-PC>
2020-10-04 14:42:28 -0700

    Changes to AddingMachine.java

--- CSE360HW2/src/cse360assignment2/AddingMachine.java
+++ CSE360HW2/src/cse360assignment2/AddingMachine.java
@@ -2,46 +2,56 @@ package cse360assignment2;
 
 public class AddingMachine {
 	private int total;
+	private String history;
 	
 	/**
-	 *   Initializes the total variable to 0
+	 *   Initializes the total variable to 0 and the history variable to "0"
 	 */
 	public AddingMachine () {
-	  total = 0;  // not needed - included for clarity
+		history = "0";
+		total = 0;  // not needed - included for clarity
 	}
 	
 	/**
-	 * Returns the total
-	 * @return
+	 * Returns the total variable
+	 * @return Returns the total variable
 	 */
 	public int getTotal () {
-	  return 0;
+		
+	  return total;
 	}
 	
 	/**
-	 *   Adds the parameter to the total variable
+	 *   Adds the parameter to the total variable and adds the transaction to the history string.
 	 * @param value adds the parameter to the total variable
 	 */
 	public void add (int value) {
+		total = total + value;
+		history = history + " + " + value;
 	}
 	
 	/**
-	 * Subtracts the parameter to from the total variable
+	 * Subtracts the parameter from the total variable and adds the transaction to the history string.
 	 * @param value subtracts the parameter from the total variable
 	 */
 	public void subtract (int value) {
+		total = total - value;
+		history = history + " - " + value;
 	}
 
 	/**
-	 *  Returns a history of the transactions in a string format
+	 *  @return Returns a history of the transactions in a string format
 	 */
 	public String toString () {
-	  return "";
+		
+	  return history;
 	}
 	
 	/**
-	 * Clears the memory
+	 * Clears the memory by restting the history and total value
 	 */
 	public void clear() {
+		history = "0";
+		total = 0;
 	}
 }
